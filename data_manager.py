3c79d37f2161c4d9766c67d89068e3d111ac63f0
David Ekeocha <davidekeocha36@gmail.com>
2026-07-28 18:51:11 +0100

    Secure Sheety credentials using .env

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,23 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.username = os.getenv("SHEETY_USERNAME")
+        self.password = os.getenv("SHEETY_PASSWORD")
+
+        self.sheety_endpoint = os.getenv("SHEETY_ENDPOINT")
+
+        self.sheety_response = requests.get(self.sheety_endpoint, auth=(self.username, self.password))
+        self.sheety_json = self.sheety_response.json()
+
+
+
+    def get_destination_data(self):
+        return self.sheety_json["prices"]
+
